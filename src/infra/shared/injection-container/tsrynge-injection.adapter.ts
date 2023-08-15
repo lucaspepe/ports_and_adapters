c2f2a8b700224be58f5e2c78d1fbbe46d667935d
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2023-08-14 21:54:10 -0300

    ajuste nos imports

--- src/infra/shared/injection-container/tsrynge-injection.adapter.ts
+++ src/infra/shared/injection-container/tsrynge-injection.adapter.ts
@@ -1,10 +1,10 @@
 import { DependencyContainer, container } from "tsyringe";
 import { DependecyInjection } from "./dependency-injection.interface";
 import TransactionRepository from "../../domain/repository/TransactionRepository";
-import TransactionMemoryRepository from "../../infra/repository/TransactionMemoryRepository";
+import TransactionMemoryRepository from "../../repository/TransactionMemoryRepository";
 // import TransactionDatabaseRepository from "../../repository/TransactionDatabaseRepository";
-import Connection from "../../infra/database/Connection";
-import PostgreSQLAdapter from "../../infra/database/PostgreSQLAdapter";
+import Connection from "../../database/Connection";
+import PostgreSQLAdapter from "../../database/PostgreSQLAdapter";
 
 class TsyringeDependencyInjectionAdapter implements DependecyInjection {
   private tsyringe: DependencyContainer;
